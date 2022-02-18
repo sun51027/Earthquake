8b6c852aac30bcb266eda9b3b367cf031274b32d
Zero <arashikattun48@gmail.com>
2022-02-18 21:03:30 +0800

    analysis done

--- Analyzer.cc
+++ Analyzer.cc
@@ -1,4 +1,4 @@
-TH1* AddHist( TDirectory *dir){
+TH1* addHist( TDirectory *dir){
 	int count = 0;
 	
 	TH1D *Template = new TH1D ("Template","",1024,-0.5,4.5);
@@ -9,7 +9,7 @@ TH1* AddHist( TDirectory *dir){
 
 	while ((keyAsObj = (TKey*) next())){
 		auto key = (TKey*) keyAsObj;
-//		cout << key->GetName() << " " << key->GetClassName() << endl;
+	//	cout << key->GetName() << " " << key->GetClassName() << endl;
 		TDirectory* dir2 = (TDirectory*)dir->Get(key->GetName());
 		dir2->cd();
 		TIter next2(dir2->GetListOfKeys());
@@ -18,7 +18,7 @@ TH1* AddHist( TDirectory *dir){
 			auto key2 = (TKey*) keyAsObj2;
 			obj = (TH1*)dir2->Get(key2->GetName()); // copy every th1 histogram to obj
 			double obj_K40 = obj->Integral();
-//			cout<<"count "<<count<<", obj_Integral() "<<obj_K40<<endl;
+	//		cout<<"count "<<count<<", obj_Integral() "<<obj_K40<<endl;
 			Template->Add(obj);
 			delete obj;
 		}
@@ -33,26 +33,15 @@ TH1* AddHist( TDirectory *dir){
 			
 
 }
-void Analyzer(){
+void writeFile(TH1 *obj,TFile* ofile){
 
-//	TCanvas *canvas = new TCanvas("canvas","",800,600);
+	ofile->cd("ScaleHist");
+	obj->Write();
+}
+void scale(TH1* Template,TFile *ofile, TDirectory *dir ){
 
-	TFile *oFile = new TFile("o.root","recreate");
-	TFile *file= new TFile("output.root");
-	TDirectory *dir = (TDirectory*)file->Get("HistoCh0");
-	dir->cd();
-	
-	//make a template
-	TH1* Template = AddHist(dir);
-	Template->Draw();
-
-	//Scale histogram to template
-	int bmin = Template->GetXaxis()->FindBin(1.3);
-	int bmax = Template->GetXaxis()->FindBin(1.5);
-	cout<<"bmin "<<bmin<<", bmax "<<bmax<<endl;
-	double K40 = Template->Integral(bmin,bmax);
+	double K40 = Template->Integral(Template->GetXaxis()->FindBin(1.3),Template->GetXaxis()->FindBin(1.5));
 	cout<<"Integral K40 "<<K40<<endl;
-	
 		
 	TKey *keyAsObj, *keyAsObj2;
 	TIter next(dir->GetListOfKeys());
@@ -60,7 +49,7 @@ void Analyzer(){
 
 	while ((keyAsObj = (TKey*) next())){
 		auto key = (TKey*) keyAsObj;
-		cout << key->GetName() << " " << key->GetClassName() << endl;
+		//cout << key->GetName() << " " << key->GetClassName() << endl;
 		TDirectory* dir2 = (TDirectory*)dir->Get(key->GetName());
 		dir2->cd();
 		TIter next2(dir2->GetListOfKeys());
@@ -70,9 +59,63 @@ void Analyzer(){
 			int bmin = obj->GetXaxis()->FindBin(1.3);
 			int bmax = obj->GetXaxis()->FindBin(1.5);
 			double obj_K40 = obj->Integral(bmin,bmax);
-			cout<<key2->GetName()<<", obj_Integral() "<<obj_K40<<endl;
+			//cout<<key2->GetName()<<", obj_Integral() "<<obj_K40<<endl;
+			double factor = K40/obj_K40;
+			obj -> Scale(factor);
+			string Name = (string)key->GetName()+(string)key2->GetName();
+			obj -> SetName(Name.c_str());
+			writeFile(obj,ofile);
 			delete obj;
 		}
 	}
+}
+TH1* doAnalysis(TH1* Template, TH1 *h_diff,TFile *ofile){
+
+	double nTemplateSig = Template->Integral(Template->GetXaxis()->FindBin(0.25),Template->GetXaxis()->FindBin(1));
+	cout<<"nSiginficant interval "<<nTemplateSig<<endl;
+
+	TH1 *obj_new ;
+	TKey *keyAsObj_new;
+	TDirectory *dir_new = (TDirectory*)ofile->Get("ScaleHist");
+	TIter next_new(dir_new->GetListOfKeys());
+
+	while ((keyAsObj_new = (TKey*)next_new() )){
+		auto key_new = (TKey*) keyAsObj_new;
+		obj_new = (TH1*)dir_new->Get(key_new->GetName());
+
+		double nDailySig = obj_new->Integral(obj_new->GetXaxis()->FindBin(0.25),obj_new->GetXaxis()->FindBin(1));
+		double diff = nDailySig-nTemplateSig;
+		//cout<<"diff "<<diff<<endl;
+		h_diff->Fill(diff);
+		delete obj_new;
+	}
+	cout<<"h_diff Standard Dev "<<h_diff-> GetStdDev()<<endl;
+	h_diff->Scale(1/h_diff->GetStdDev());
+        h_diff->Draw();	
+	return h_diff;
+
+}
+void Analyzer(){
+
+	TCanvas *canvas = new TCanvas("canvas","",800,600);
+
+	TFile *ofile = new TFile("o.root","recreate");
+	ofile->mkdir("ScaleHist");
+
+	TFile *file= new TFile("output.root");
+	TDirectory *dir = (TDirectory*)file->Get("HistoCh0");
+	dir->cd();
+	
+	//make a template
+	TH1* Template = addHist(dir);
+
+	//Scale histogram to template
+	scale(Template,ofile,dir);
+
+	//do analysis
+	TH1D *h_diff = new TH1D("h_diff","",100,-30000000,30000000);
+	doAnalysis(Template,h_diff,ofile);
+	canvas->SaveAs("h_diff.pdf");
+
 
 }
