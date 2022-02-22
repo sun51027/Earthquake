cab5add2edd04b4057adb23945132766359a9978
Zero <arashikattun48@gmail.com>
2022-02-22 23:40:12 +0800

    set bins 0 as mean

--- Analyzer.cc
+++ Analyzer.cc
@@ -19,7 +19,8 @@ TH1* addHist( TDirectory *dir){
 			auto key2 = (TKey*) keyAsObj2;
 			obj = (TH1*)dir2->Get(key2->GetName()); // copy every th1 histogram to obj
 			double obj_K40 = obj->Integral();
-			Template->Add(obj);
+			Template->Add(obj);	
+
 			delete obj;
 		}
 	}
@@ -34,15 +35,36 @@ TH1* addHist( TDirectory *dir){
 }
 void writeFile(TH1 *obj,TFile* ofile){
 
-	ofile->cd("ScaleHist");
+	ofile->cd("AdjustedHist");
 	obj->Write();
+}
+TH1* setZeroBinContent(TH1 *hist){
+
+
+	for(int i = 0;i<hist->GetNbinsX();i++){
+	
+		//cout<<i<<" GetBinContent "<<hist->GetBinContent(i);
+		if(hist->GetBinContent(i) == 0 && hist->GetBinContent(i+1) == 0 && hist->GetBinContent(i+2) == 0){
+			//cout<<"\t\t ,  "<<hist->GetBinContent(i)<<endl;
+		    continue;
+		}
+		else if( hist->GetBinContent(i) == 0){
+		
+			hist->SetBinContent(i,(hist->GetBinContent(i-1)+hist->GetBinContent(i+1))/2);		
+
+		}
+			//cout<<"\t\t ,  "<<hist->GetBinContent(i)<<endl;
+	}
+
+
+	return hist;
 }
   const double minK40 = 1.3;
   const double maxK40 = 1.5;
   const double minRadon = 0.25;
   const double maxRadon = 0.8;
 
-TH1* doAnalysis(TH1* Template, TH1 *h_diff,TDirectory *dir){
+TH1* doAnalysis(TH1* Template, TH1 *h_diff,TDirectory *dir, TFile *ofile){
 
 	double K40 = Template->Integral(Template->GetXaxis()->FindBin(minK40),Template->GetXaxis()->FindBin(maxK40));	
 
@@ -61,19 +83,21 @@ TH1* doAnalysis(TH1* Template, TH1 *h_diff,TDirectory *dir){
 
 		  auto key2 = (TKey*) keyAsObj2;
 		  obj = (TH1*)dir2->Get(key2->GetName()); // copy every th1 histogram to obj
+  		  obj = setZeroBinContent(obj);
 		  double obj_K40 = obj->Integral(obj->GetXaxis()->FindBin(minK40),obj->GetXaxis()->FindBin(maxK40));
 
 		  if(obj_K40 > 0){
 
 			double factor = obj_K40/K40;
+			//cout<<"factor "<<factor<<endl;
 			TH1D *ScaledTemplate = (TH1D*)(Template->Clone("ScaledTemplate"));
 			ScaledTemplate -> Scale(obj_K40/K40); // template scale to same as the hourly plot
 
 			double nTemplateSig = ScaledTemplate->Integral(ScaledTemplate->GetXaxis()->FindBin(minRadon),ScaledTemplate->GetXaxis()->FindBin(maxRadon));
 			double nDailySig = obj->Integral(obj->GetXaxis()->FindBin(minRadon),obj->GetXaxis()->FindBin(maxRadon));
 			double diff = nDailySig-nTemplateSig;
+			//cout<<"diff "<<diff<<endl;
 			h_diff->Fill(diff);
-
 			delete obj;
 			delete ScaledTemplate;
 		  }
@@ -87,7 +111,7 @@ void Analyzer(){
 	TCanvas *canvas = new TCanvas("canvas","",800,600);
 
 	TFile *ofile = new TFile("o.root","recreate");
-	ofile->mkdir("ScaleHist");
+	ofile->mkdir("AdjustedHist");
 
 	TFile *file= new TFile("output.root");
 	TDirectory *dir = (TDirectory*)file->Get("HistoCh0");
@@ -98,11 +122,23 @@ void Analyzer(){
 	Template->Draw();
 	canvas->SaveAs("template.pdf");
 
+	// check zero entry bin
+	Template = setZeroBinContent(Template);
+	Template->Draw();
+	canvas->SaveAs("template_adjust.pdf");
+
 	//do analysis
 	TH1D *h_diff = new TH1D("h_diff","",100,-10000,10000);
-	doAnalysis(Template,h_diff,dir);
+	doAnalysis(Template,h_diff,dir,ofile);
+	
+	//Fitting to get sigma
+	
+//	TF1 *f1  = new TF1("f1",TMath::Gaus(x,[0]/*mean*/,[1]/*sigma*/),-10000,10000);
+//	f1->SetParameter(-17);
+//	f1->SetParameter(3000);
+	h_diff->Fit("gaus","L");
     h_diff->Draw();	
-	canvas->SaveAs("h_diff.pdf");
+//	canvas->SaveAs("h_diff.pdf");
 
 
 }
