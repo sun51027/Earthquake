58408f138805f49fb2fde7a799b3999dc33e6741
Zero <arashikattun48@gmail.com>
2022-03-24 18:04:20 +0800

    update

--- Analyzer.cc
+++ Analyzer.cc
@@ -1,70 +1,24 @@
 #include <iostream>
-
+#include "interface/EQ.h"
 #include "TCanvas.h"
 #include "TDirectory.h"
 #include "TF1.h"
 #include "TFile.h"
 #include "TGraph.h"
+#include "TMultiGraph.h"
 #include "TH1D.h"
 #include "TKey.h"
+#include "TStyle.h"
 
 using namespace std;
-void writeFile(TH1 *obj, TFile *ofile)
-{
-  ofile->cd("AdjustedHist");
-  obj->Write();
-}
+const double Earthquake::minK40   = 1.3;
+const double Earthquake::maxK40   = 1.5;
+const double Earthquake::minRadon = 0.25;
+const double Earthquake::maxRadon = 0.8;
 
-TH1 *setZeroBinContent(TH1 *hist)
-{
-  for (int i = 0; i < hist->GetNbinsX(); i++) {
-    if (hist->GetBinContent(i) == 0 && hist->GetBinContent(i + 1) == 0 && hist->GetBinContent(i + 2) == 0) {
-      continue;
-    } else if (hist->GetBinContent(i) == 0) {
-      hist->SetBinContent(i, (hist->GetBinContent(i - 1) + hist->GetBinContent(i + 1)) / 2);
-    }
-  }
-
-  return hist;
-}
-
-double FittingScale(TH1 *h_diff)
-{
-  TCanvas *canvas = new TCanvas("canvas", "", 800, 600);
-  h_diff->Fit("gaus");
-  h_diff->Draw();
-  canvas->SaveAs("h_fitting_gaus.pdf");
-
-  // h_diff scale yields
-  TH1D *h_scale = new TH1D("h_scale", "", 100, -10000, 10000);
-  for (int i = 0; i < 100; i++) {
-    h_scale->SetBinContent(i + 1, h_diff->GetBinContent(i + 1) / (h_diff->GetFunction("gaus")->GetParameter(2)));
-  }
-  h_scale->Draw();
-  h_scale->SetStats(0);
-  canvas->SaveAs("h_scale.pdf");
-
-  double sigma = h_diff->GetFunction("gaus")->GetParameter(2);
-  return sigma;
-}
-
-double calibration(TH1 *obj)
+void Earthquake::doAnalysis(TH1 *Template, TH1 *h_diff, TDirectory *dir, TFile *ofile)
 {
 
-  obj->Fit("gaus", "R", "", 2.1, 2.4);
-  double cfactor = obj->GetFunction("gaus")->GetParameter(1) / 2.23118;
-
-  cout << cfactor << endl;
-  return cfactor;
-}
-
-const double minK40   = 1.3;
-const double maxK40   = 1.5;
-const double minRadon = 0.25;
-const double maxRadon = 0.8;
-
-void doAnalysis(TH1 *Template, TH1 *h_diff, TDirectory *dir, TFile *ofile)
-{
   TCanvas *c   = new TCanvas("canvas", "", 800, 600);
   double   K40 = Template->Integral(Template->GetXaxis()->FindBin(minK40), Template->GetXaxis()->FindBin(maxK40));
 
@@ -72,15 +26,14 @@ void doAnalysis(TH1 *Template, TH1 *h_diff, TDirectory *dir, TFile *ofile)
   TIter    next(dir->GetListOfKeys());
   TH1     *obj;
   Double_t x[4000], y[4000];
-  Double_t cfactor[4000];
-  Int_t    n = 2500;
-  int      i = 0;
+  double   cfactor[4000];
+  double   cali_peak[4000];
+  double   uncali_peak[4000];
+  int      N = 0;
   int      h = 0; // # of hour
 
   TString hist_name[2500];
-
-  double testx[4000];
-  double testy[4000];
+  TString daily_name[2500];
 
   while ((keyAsObj = (TKey *)next())) {
     auto        key  = (TKey *)keyAsObj;
@@ -90,111 +43,145 @@ void doAnalysis(TH1 *Template, TH1 *h_diff, TDirectory *dir, TFile *ofile)
 
     while ((keyAsObj2 = (TKey *)next2())) {
 
-      if (h > 1210) { // start from 9/14 (h=1211)
+      auto key2 = (TKey *)keyAsObj2;
+      obj       = (TH1 *)dir2->Get(key2->GetName()); // copy every th1 histogram to
+      obj       = setZeroBinContent(obj);            // fill the empty bin with average of adjacent bins
 
-        auto key2 = (TKey *)keyAsObj2;
-        obj       = (TH1 *)dir2->Get(key2->GetName()); // copy every th1 histogram to
-        obj       = setZeroBinContent(obj);            // fill the empty bin with average of adjacent bins
+      if (h > 1799 && obj->Integral() != 0) { // start from 9/15 h=1800)
 
-        // calibrate every hour
-        cfactor[i] = calibration(obj);
+        // set hist name ex. 12/25
+        hist_name[N].Form("%s", key->GetName());
+        hist_name[N].Remove(0, 4);
+        hist_name[N].Insert(2, "/");
+        //
+        daily_name[N].Form("%s%s", key->GetName(), key2->GetName());
+        daily_name[N].Remove(10, 4); // 20211225 22
+
+        // calibrate plots every 2 hours, get the cali factor
+        cfactor[N] = calibration(obj, ofile, daily_name[N]);
 
         // Normalize template to yiels of every hour
-        double obj_K40        = obj->Integral(obj->GetXaxis()->FindBin(minK40), obj->GetXaxis()->FindBin(maxK40));
-        double factor         = obj_K40 / K40;
-        TH1D  *ScaledTemplate = (TH1D *)(Template->Clone("ScaledTemplate"));
+        double obj_K40 = obj->Integral(obj->GetXaxis()->FindBin(minK40), obj->GetXaxis()->FindBin(maxK40));
+
+        TH1D *ScaledTemplate = (TH1D *)(Template->Clone("ScaledTemplate"));
         ScaledTemplate->Scale(obj_K40 / K40); // template scale to same as the hourly plot
+
+        // calibrate hourly K40 peak
+        //        TH1D *obj_cali = (TH1D *)(obj->Clone("obj_cali"));
+        //        for (int j = 0; j < 1024; j++) {
+        //          obj_cali->SetBinContent(j + 1, (obj->GetBinContent(j + 1)) * cfactor[N]);
+        //          // cout<<obj->GetBinContent(j+1)<<"\t"<< cfactor[N] <<"\t"<< obj_cali->GetBinContent(j+1)<<endl;
+        //        }
+
+        // input cfactor and fit K40 to get mean(peak), to see the stability.
+        // return peak of K40 hourly
+        uncali_peak[N] = stability_K40(obj, ofile, daily_name[N]);
+        //        cali_peak[N]   = stability_K40(obj_cali, ofile, daily_name[N]);
+
         double nTemplateSig = ScaledTemplate->Integral(ScaledTemplate->GetXaxis()->FindBin(minRadon),
                                                        ScaledTemplate->GetXaxis()->FindBin(maxRadon));
-        double nDailySig    = obj->Integral(obj->GetXaxis()->FindBin(minRadon), obj->GetXaxis()->FindBin(maxRadon));
-        double diff         = nDailySig - nTemplateSig;
-
-        hist_name[i].Form("%s", key->GetName());
-        hist_name[i].Remove(0, 4);
-        hist_name[i].Insert(2, "/");
-
-        x[i] = (double)(i + 1); // number of 2hour
-        y[i] = diff;
-
-        i++;
+        double nDailySig =
+          obj->Integral(obj->GetXaxis()->FindBin(minRadon), obj->GetXaxis()->FindBin(maxRadon));
+        double diff = nDailySig - nTemplateSig;
+        x[N] = (double)(N + 1); // number of 2hour
+        y[N] = diff;
         h_diff->Fill(diff);
 
         delete obj;
         delete ScaledTemplate;
+        N++;
       }
       h++;
     }
   }
-	cout<<"h "<<h<<endl;
-	cout<<"i "<<i<<endl;
 
-  TGraph *gr = new TGraph(n, x, y);
+	h_diff->SetStats(0);
+//	h_diff->Fit("gaus");
+//	h_diff->GetFunction("gaus")->GetParameter(2);
+//	h_diff->Scale(h_diff->GetFunction("gaus")->GetParameter(2));
+	h_diff->Draw();
+	c->SaveAs("h_diff.pdf");
 
-  for (int i = 0; i < h/100; i++) {
-    gr->GetXaxis()->SetBinLabel(i * 100 + 1, hist_name[i * 100]);
+  TGraph *gr = new TGraph(N, x, y);
+  gr->GetXaxis()->SetLimits(0, N);
+  for (int i = 0; i <= N / 60; i++) {
+    gr->GetXaxis()->SetBinLabel(i * 60 + 1, hist_name[i * 60]);
+  //  cout << i << " " << gr->GetXaxis()->GetBinLabel(i * 60 + 1) << endl;
   }
-
-  //   for (int i = 0; i < n; i++) {
-  //      cout << hist_name[i]<<" "<< i << " " << gr->GetPointX(i) << "\t" << gr->GetPointY(i) << " GetBinLabel "<<
-  //      gr->GetXaxis()->GetBinLabel(i+1)<<endl;
-  //   }
-  gr->GetXaxis()->SetLimits(0,h);
-
+	gr->SetTitle("");
   gr->Draw("AP");
-  c->SetGrid();
   c->SaveAs("DiffvsTime.pdf");
+  delete c;
 
-  TCanvas *c2 = new TCanvas("c2", "", 800, 600);
-
-  TGraph *corr = new TGraph(n, x, cfactor);
-  for (int i = 0; i < 25; i++) {
-    corr->GetXaxis()->SetBinLabel(i * 100 + 1, hist_name[i * 100]);
+  // draw corr factor
+  TCanvas *c2   = new TCanvas("c2", "", 900, 600);
+  TGraph  *corr = new TGraph(N, x, cfactor);
+  for (int i = 0; i <= N / 60; i++) {
+    corr->GetXaxis()->SetBinLabel(i * 60 + 1, hist_name[i * 60]);
   }
-  //  for (int i = 0; i < n; i++) {
-  //    cout << hist_name[i] << " " << i << " " << cfactor[i] << "\t" << corr->GetPointY(i) << " GetBinLabel "
-  //         << corr->GetXaxis()->GetBinLabel(i + 1) << endl;
-  //  }
-  //	corr->SetMarkerStyle(20);
+	corr->SetTitle("");
   corr->Draw("AP");
-  corr->GetXaxis()->SetLimits(0, 2500);
-  c2->SetGrid(1, 1);
-  c2->SaveAs("Corr_factor.pdf");
-  //--------------debug-----------------
-  /*
-    TGraph *test = new TGraph(t,testy,testx);
-  //	test->Draw("AP");
-  //	c2->SaveAs("test.pdf");
+  corr->GetXaxis()->SetLimits(0, N);
+	corr->GetYaxis()->SetTitle("calibration factor");
+  c2->SetGridy(1);
+  c2->SaveAs("cfactor.pdf");
+  delete c2;
 
-    TH1D* h_test = new TH1D("h_test","",2500,0,2500);
-    for(int i = 0 ; i < n ; i++){
-      h_test -> SetBinContent(i+1,cfactor[i]);
-    }
-  //	h_test->Draw();
-  //	c2->SaveAs("h_test.pdf");
-  */
-  //	FittingScale(h_diff);
+  // see if K40 is stable around 1.4 MeV (peak)after calibration
+  ////  TMultiGraph *mg = new TMultiGraph();
+  //
+  //  TGraph *stable = new TGraph(N, x, cali_peak);
+  //  stable->SetMarkerColor(kRed);
+  //  stable->SetMarkerSize(8);
+  //  stable->GetXaxis()->SetLimits(0, N);
+  //    stable->SetMaximum(1.43);
+  //    stable->SetMinimum(1.37);
+
+  TCanvas     *c3 = new TCanvas("c3", "", 900, 600);
+  TGraph *stable_uncali = new TGraph(N, x, uncali_peak);
+  stable_uncali->SetMarkerColor(kBlue);
+  stable_uncali->SetMarkerSize(8);
+  stable_uncali->GetXaxis()->SetLimits(0, N);
+	stable_uncali->GetYaxis()->SetTitle("K40 peak (MeV)");
+  stable_uncali->Draw("AP");
+  stable_uncali->SetMaximum(1.43);
+  stable_uncali->SetMinimum(1.38);
+
+  for (int i = 0; i <= N / 60; i++) {
+    stable_uncali->GetXaxis()->SetBinLabel(i * 60 + 1, hist_name[i * 60]);
+  }
+  //  mg->Add(stable);
+  //  mg->Add(stable_uncali);
+  //  mg->Draw("AP");
+	
+  c3->SetGridy(1);
+  c3->SaveAs("K40_stability.pdf");
+  delete c3;
+  //  for (int i = 0; i < N; i++) {
+  //		cout<<stable->GetPointY(i)<<endl;
+  //  }
 }
-// void Analyzer(){
 int main()
 {
-
-  TFile *ofile = new TFile("o.root", "recreate");
-  ofile->mkdir("AdjustedHist");
-
+  // outputfile
+  TFile *ofile = new TFile("oAnalyzer.root", "recreate");
+  ofile->mkdir("cali_Hist");
+  ofile->mkdir("stability_K40");
+  ofile->cd();
+  // inputfile
   TFile      *fin1 = new TFile("output.root");
   TDirectory *dir  = (TDirectory *)fin1->Get("HistoCh0");
   dir->cd();
 
   // get template
-  TFile *fin2 = new TFile("template.root");
-  fin2->cd();
-  TH1 *Template = (TH1 *)fin2->Get("Template");
+  TFile *fin2     = new TFile("template.root");
+  TH1   *Template = (TH1 *)fin2->Get("Template");
+
+  Earthquake EQ;
 
   // do analysis
   TH1D *h_diff = new TH1D("h_diff", "", 100, -10000, 10000);
-  doAnalysis(Template, h_diff, dir, ofile);
-
-  // fitting
+  EQ.doAnalysis(Template, h_diff, dir, ofile);
 
   return 0;
 }
--- MakeTemplate.cc
+++ MakeTemplate.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-
 #include "TCanvas.h"
 #include "TDirectory.h"
 #include "TF1.h"
@@ -17,10 +16,17 @@
 #include "RooArgList.h"
 #include "RooPlot.h"
 #include "RooDataHist.h"
+
+#include "interface/EQ.h"
+
 using namespace RooFit;
 using namespace std;
 
-TH1 *addHist(TDirectory *dir)
+const double Earthquake::minK40   = 1.3;
+const double Earthquake::maxK40   = 1.5;
+const double Earthquake::minRadon = 0.25;
+const double Earthquake::maxRadon = 0.8;
+TH1* Earthquake::addHist(TDirectory *dir)
 {
   int count = 0;
 
@@ -55,72 +61,27 @@ TH1 *addHist(TDirectory *dir)
   cout << count << endl;
   return Template;
 }
-TH1 *setZeroBinContent(TH1 *hist)
-{
-  for (int i = 0; i < hist->GetNbinsX(); i++) {
-    if (hist->GetBinContent(i) == 0 && hist->GetBinContent(i + 1) == 0 && hist->GetBinContent(i + 2) == 0) {
-      continue;
-    } else if (hist->GetBinContent(i) == 0) {
-      hist->SetBinContent(i, (hist->GetBinContent(i - 1) + hist->GetBinContent(i + 1)) / 2);
-    }
-  }
 
-  return hist;
-}
-void calibration(TH1 *Template, TFile *ofile)
-{
 
-  TCanvas *c    = new TCanvas("c", "", 800, 600);
-  double   xmin = Template->GetXaxis()->FindBin(2.1);
-  double   xmax = Template->GetXaxis()->FindBin(2.4);
-  cout << "xmax " << xmax << endl;
-  cout << "xmin " << xmin << endl;
-  cout << "# of bins " << xmax - xmin + 1 << endl;
-  TH1D *cal = new TH1D("cal", "", xmax - xmin + 1, 2.1, 2.4);
-  for (int i = 0; i < xmax - xmin + 1; i++) {
-    cal->SetBinContent(i + 1, Template->GetBinContent(xmin + i));
-    //    cout << "Template->GetBinContent(" << xmin + i << ") " << Template->GetBinContent(xmin + i) << endl;
-  }
-  // Fitting
-  // observable
-  RooRealVar x("x", "random variable", 2.1, 2.4);
-
-  // Gaussian model
-  RooRealVar  mu("mu", "mean parameter", 2.2, 2.1, 2.4);
-  RooRealVar  sigma("sigma", "width parameter", 0.1, 0.0, 0.3);
-  RooGaussian gaus("gaus", "Gaussian PDF", x, mu, sigma);
-
-  // Linear function: 1 + slope*x
-  RooRealVar    slope("slope", "slope parameter", -1000, -100000, 100000);
-  RooPolynomial linear("linear", "Linear function", x, RooArgSet(slope));
-  // Expo
-  RooRealVar     par("par", "", -1, -10, 10);
-  RooExponential expo("expo", "", x, par);
-  // add up: Gaussian + linear
-  RooRealVar fraction("fraction", "fraction of Gaussian", 0.5, 0., 1.);
-  RooAddPdf  model("model", "PDF model", RooArgList(gaus, expo), RooArgList(fraction));
-  // RooAddPdf  model("model", "PDF model", RooArgList(gaus, linear), RooArgList(fraction));
-  RooDataHist data("data", "data", x, Import(*cal));
-  RooPlot    *frame = x.frame();
-  data.plotOn(frame);
-  model.fitTo(data);
-  model.plotOn(frame);
-  frame->Draw();
-  c->SaveAs("cal_new.pdf");
-  //  return cfactor;
-}
 int main()
 {
 
-  TFile *ofile = new TFile("o.root", "recreate");
-  ofile->mkdir("AdjustedHist");
+  TFile *ofile = new TFile("template.root", "recreate");
+  ofile->mkdir("stability_K40");
+  ofile->mkdir("cali_Hist");
 
   TFile      *file = new TFile("output.root");
   TDirectory *dir  = (TDirectory *)file->Get("HistoCh0");
   dir->cd();
+	// init
+	Earthquake EQ;
 
   // make a template
-  TH1 *Template = addHist(dir);
-  Template      = setZeroBinContent(Template);
-  calibration(Template, ofile);
+  TH1 *Template;
+ 	Template	= EQ.addHist(dir);
+  Template      = EQ.setZeroBinContent(Template);
+	ofile->cd();
+	Template->Write("Template");
+  EQ.calibration(Template, ofile,"Template_cali");
+	EQ.stability_K40(Template,ofile,"Template_stab");
 }
