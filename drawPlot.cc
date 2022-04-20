8a96c9b10deab35ba83f06e8e769e7ed7e675d64
Zero <arashikattun48@gmail.com>
2022-04-20 15:06:28 +0800

    read timename successfully & draw graph

--- drawPlot.cc
+++ drawPlot.cc
@@ -1,7 +1,10 @@
 // STL
 using namespace std;
 #include <iostream>
-
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
 // my header
 #include "interface/EQ.h"
 
@@ -19,28 +22,48 @@ using namespace std;
 #include "TStyle.h"
 #include "rootlogon.h"
 // using namespace mgr;
-void drawPlot(){
 
-	TFile *fin = new TFile("oAnalyzer.root");
-	TDirectory *dir = (TDirectory*)fin->Get("Analysis_plot");
-	dir->cd();
-	
-	// difference vs time
+void drawPlot()
+{
+
+  // load root
+  TFile      *fin = new TFile("oAnalyzer.root");
+  TDirectory *dir = (TDirectory *)fin->Get("Analysis_plot");
+  dir->cd();
+
+  // load plot_name.txt
+  ifstream ifs;
+  ifs.open("plot_name.txt", std::ios::in);
+  vector<TString> time;
+  if (!ifs.is_open()) {
+    cout << "fail to open file." << endl;
+  } else {
+    while (!ifs.eof()) {
+      string s;
+      getline(ifs, s);
+      time.push_back(s);
+    }
+  }
+  ifs.close();
+
   TCanvas *c = new TCanvas("canvas", "", 800, 600);
-	TGraph *gr = (TGraph*)dir->Get("DiffvsTime");
-//  gr->GetXaxis()->SetLimits(0, N);
-//  for (int i = 0; i <= N / 60; i++) {
-//    gr->GetXaxis()->SetBinLabel(i * 60 + 1, hist_name[i * 60]);
-//  }
+
+  // difference vs time
+  TGraph *gr = (TGraph *)dir->Get("g_diffvsTime");
+  gr->GetXaxis()->SetLimits(0, time.size());
+  for (int i = 0; i <= time.size() / 60; i++) {
+    gr->GetXaxis()->SetBinLabel(i * 60 + 1, time[i * 60]);
+  }
   gr->SetTitle("");
   gr->Draw("AP");
-  c->SaveAs("plots/DiffvsTime.pdf");
+  c->SaveAs("DiffvsTime.pdf");
 
-	// Correlation factor
-//  corr->SetTitle("");
-//  corr->Draw("AP");
-//  corr->GetXaxis()->SetLimits(0, N);
-//  corr->GetYaxis()->SetTitle("Calibration factor");
-//  c2->SetGridy(1);
-//  c2->SaveAs("plots/cfactor_beforSep.pdf");
+  //   Correlation factor
+  TGraph *corr = (TGraph *)dir->Get("g_cfactor");
+  corr->SetTitle("");
+  corr->Draw("AP");
+  corr->GetXaxis()->SetLimits(0, time.size());
+  corr->GetYaxis()->SetTitle("Calibration factor");
+  c->SetGridy(1);
+  c->SaveAs("cfactor.pdf");
 }
