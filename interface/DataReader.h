acbdcc3840610e1f863d083750fc7c8541fbaa81
Zero <arashikattun48@gmail.com>
2022-05-20 18:45:24 +0800

    construction

--- interface/DataReader.h
+++ interface/DataReader.h
@@ -11,41 +11,58 @@ using namespace std;
 class DataReader {
 public:
   DataReader() {}
-  ~DataReader() {}
-	void EarthquakeDirectory();
+  DataReader(double lat, double lon, double depth, double ML, double nstn, double dmin, double gap, double trms,
+             double ERH, double ERZ, double nph)
+    : lat_(lat), lon_(lon), depth_(depth), ML_(ML), nstn_(nstn), dmin_(dmin), gap_(gap), trms_(trms), ERH_(ERH),
+      ERZ_(ERZ), nph_(nph) {}
 
-  double         RichterML[4000];
-  double         Depth[4000];
+  ~DataReader() {}
+  void EarthquakeDirectory();
+
+  double RichterML[4000];
+  double Depth[4000];
 
 private:
-  vector<string> date_;
-  vector<string> time_;
-  vector<string> fixed;
-  vector<string> quality;
+  vector<string> date_raw;
+  vector<string> time_raw;
+  vector<string> fixed_raw;
+  vector<string> quality_raw;
 
-  vector<string> lat_;
-  vector<string> lon_;
-  vector<string> depth_;
-  vector<string> ML_;
-  vector<string> nstn_;
-  vector<string> dmin_;
-  vector<string> gap_;
-  vector<string> trms_;
-  vector<string> ERH_;
-  vector<string> ERZ_;
-  vector<string> nph_;
+  vector<string> lat_raw;
+  vector<string> lon_raw;
+  vector<string> depth_raw;
+  vector<string> ML_raw;
+  vector<string> nstn_raw;
+  vector<string> dmin_raw;
+  vector<string> gap_raw;
+  vector<string> trms_raw;
+  vector<string> ERH_raw;
+  vector<string> ERZ_raw;
+  vector<string> nph_raw;
 
-  vector<double> lat;
-  vector<double> lon;
-  vector<double> depth;
-  vector<double> ML;
-  vector<double> nstn;
-  vector<double> dmin;
-  vector<double> gap;
-  vector<double> trms;
-  vector<double> ERH;
-  vector<double> ERZ;
-  vector<int>    nph;
+  double lat_;
+  double lon_;
+  double depth_;
+  double ML_;
+  double nstn_;
+  double dmin_;
+  double gap_;
+  double trms_;
+  double ERH_;
+  double ERZ_;
+  double nph_;
+
+  //  vector<double> lat;
+  //  vector<double> lon;
+  //  vector<double> depth;
+  //  vector<double> ML;
+  //  vector<double> nstn;
+  //  vector<double> dmin;
+  //  vector<double> gap;
+  //  vector<double> trms;
+  //  vector<double> ERH;
+  //  vector<double> ERZ;
+  //  vector<int>    nph;
 };
 
 #endif
