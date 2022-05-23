342621269bfa0f448b646e228c3d1de7be2dad18
Zero <arashikattun48@gmail.com>
2022-05-23 18:11:44 +0800

    merge header with cc temporarily for test

--- ReadData.cc
+++ ReadData.cc
@@ -13,9 +13,67 @@ using namespace std;
 #include "TGraph.h"
 #include "TCanvas.h"
 
-#include "interface/DataReader.h"
+//#include "interface/DataReader.h"
 
 // void DataReader::EarthquakeDirectory(){
+class DataReader {
+public:
+  DataReader() {}
+  DataReader(double lat, double lon, double depth, double ML, double nstn, double dmin, double gap, double trms,
+             double ERH, double ERZ, double nph)
+    : lat_(lat), lon_(lon), depth_(depth), ML_(ML), nstn_(nstn), dmin_(dmin), gap_(gap), trms_(trms), ERH_(ERH),
+      ERZ_(ERZ), nph_(nph) {}
+
+  ~DataReader() {}
+  void EarthquakeDirectory();
+
+  double RichterML[4000];
+  double Depth[4000];
+
+//private:
+  vector<string> date_raw;
+  vector<string> time_raw;
+  vector<string> fixed_raw;
+  vector<string> quality_raw;
+
+  vector<string> lat_raw;
+  vector<string> lon_raw;
+  vector<string> depth_raw;
+  vector<string> ML_raw;
+  vector<string> nstn_raw;
+  vector<string> dmin_raw;
+  vector<string> gap_raw;
+  vector<string> trms_raw;
+  vector<string> ERH_raw;
+  vector<string> ERZ_raw;
+  vector<string> nph_raw;
+
+  double lat_;
+  double lon_;
+  double depth_;
+  double ML_;
+  double nstn_;
+  double dmin_;
+  double gap_;
+  double trms_;
+  double ERH_;
+  double ERZ_;
+  double nph_;
+
+    vector<double> lat;
+    vector<double> lon;
+    vector<double> depth;
+    vector<double> ML;
+    vector<double> nstn;
+    vector<double> dmin;
+    vector<double> gap;
+    vector<double> trms;
+    vector<double> ERH;
+    vector<double> ERZ;
+    vector<double> nph;
+};
+
+
 int main()
 {
 
@@ -154,7 +212,7 @@ int main()
     //		cout<< "N["<<j<<"] "<<N[j]<<endl;
   }
   for (int rn = 0; rn < datetime_Rn.size(); rn++) {
-    cout << "Test ML " << Test[rn].ML_ << endl;
+    cout << "Test ML "<<rn<< " "<< Test[rn].ML_ << endl;
   }
 
   return 0;
