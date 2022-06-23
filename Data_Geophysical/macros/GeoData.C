699ed52679a0ff1b53af92f25ed8a4cbf09c2837
Zero <arashikattun48@gmail.com>
2022-06-23 15:41:00 +0800

    time = =

---
+++ Data_Geophysical/macros/GeoData.C
@@ -0,0 +1,86 @@
+void GeoData(string infileName)
+{
+
+  TCanvas *c     = new TCanvas("c", "", 800, 600);
+  TFile *infile = new TFile(infileName.c_str(), "READ");
+  TTree *fChain = (TTree *)infile->Get("GeoData");
+  // Declaration of leaf types
+  Float_t data;
+  Int_t   timestamp;
+  Int_t   timestamp_ns;
+  // List of branches
+  TBranch *b_data;         //!
+  TBranch *b_timestamp;    //!
+  TBranch *b_timestamp_ns; //!
+  fChain->SetBranchAddress("data", &data, &b_data);
+  fChain->SetBranchAddress("timestamp", &timestamp, &b_timestamp);
+  fChain->SetBranchAddress("timestamp_ns", &timestamp_ns, &b_timestamp_ns);
+
+  std::vector<float> data_collection;
+  std::vector<float> ts_collection;
+  float              datatmp = 0;
+  Long64_t           timetmp = 0;
+  int                n       = 0;
+
+  TString s_year  = infileName;
+  TString s_month = infileName;
+  TString s_day   = infileName;
+  s_year.Remove(0, 23);
+  s_year.Remove(4, 18);
+  s_month.Remove(0, 27);
+  s_month.Remove(2, 16);
+  s_day.Remove(0, 29);
+  s_day.Remove(2, 14);
+  cout << "year " << stoi(string(s_year)) << endl;
+  cout << "mon " << stoi(string(s_month)) << endl;
+  cout << "day " << stoi(string(s_day)) << endl;
+
+  if (fChain == 0) return;
+
+  Long64_t nentries = fChain->GetEntriesFast();
+  for (Long64_t ientry = 0; ientry < nentries; ientry++) {
+
+    fChain->GetEntry(ientry);
+    if (ientry % 10000000 == 0)
+      std::cout << ientry << " " << std::setprecision(3) << float(ientry) / float(nentries) * 100 << "%" << std::endl;
+
+    if (ientry > 0 && timestamp == timetmp) {
+      if (abs(data) > abs(datatmp)) datatmp = data;
+    } else {
+      // include ientry = 0, that is in first entry we set timetmp = timestamp
+      //    cout << "timestamp " << timestamp << " timetmp " << timetmp <<" timestamp_ns "<<timestamp_ns<<endl;
+      timetmp = timestamp;
+      data_collection.push_back(datatmp); // input data every second
+      n++;
+      ts_collection.push_back(timestamp + timestamp_ns * 1e-9);
+      datatmp = 0;
+    }
+
+    // if (Cut(ientry) < 0) continue;
+  }
+//  TDatime da(1970, 1,1, 00, 00, 00);
+//  gStyle->SetTimeOffset(da.Convert());
+  TGraph *gdata = new TGraph(data_collection.size(), ts_collection.data(), data_collection.data());
+  gdata->Draw("AL");
+  gdata->SetMarkerStyle(20);
+  gdata->GetXaxis()->SetTimeDisplay(1);
+  gdata->GetXaxis()->SetTimeOffset(1);
+  gdata->GetXaxis()->SetTimeFormat("%m/%d");
+//	gdata->GetXaxis()->SetNdivisions(520);
+//  gdata->GetXaxis()->SetLimits(0,n);
+  c->SetGrid(1,0);
+	c->Modified();
+  c->SaveAs("EQ"+s_year+s_month+s_day+".png");
+  cout << "nentries " << nentries << endl;
+  cout << "size " << data_collection.size() << endl;
+  cout << "size " << ts_collection.size() << endl;
+  cout << "n " << n << endl;
+  //   TFile* f = new TFile("test.root","RECREATE");
+  //   gdata->Write();
+  //   f->Write();
+  //   f->Close();
+  //  return;
+  // TDatime da(stoi(string(s_year)), stoi(string(s_month)), stoi(string(s_day)), 00, 00, 00);
+	cout<<"1st timestamp "<<(int)ts_collection[0]<<endl;
+	cout<<"final timestamp "<<(int)ts_collection[nentries-2]<<endl;
+}
